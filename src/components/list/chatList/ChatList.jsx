c87d1bbb1adb0693e144b251ff0cccd4ac6e433f
DhruvPokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2025-07-08 13:05:05 +0530

    style: Style the top part of the Chat List

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,11 +1,22 @@
-export default function ChatList(){
-    return <div className="chatList">
-        <div className="search">
-            <div className="searchBar">
-                <img src="/search.png"></img>
-                <input type="text" placeholder="Search"></input>
+import { useState } from "react";
+import "./chatList.css";
+
+export default function ChatList() {
+    const [addMode, setAddMode] = useState(false);
+
+    return (
+        <div className="chatList">
+            <div className="search">
+                <div className="searchBar">
+                    <img src="/search.png"></img>
+                    <input type="text" placeholder="Search"></input>
+                </div>
+                <img
+                    src={addMode ? "./minus.png" : "./plus.png"}
+                    className="add"
+                    onClick={() => setAddMode((prev) => !prev)}
+                ></img>
             </div>
-            <img src="/plus.png"></img>
         </div>
-    </div>
+    );
 }
