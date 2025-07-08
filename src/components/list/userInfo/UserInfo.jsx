34032e1d4f24b1cc7d96f834ddf4407b413afcca
cs24b010 <cs24b010@iittp.ac.in>
2025-07-08 06:40:34 +0000

    Setup UserInfo

--- src/components/list/userInfo/UserInfo.jsx
+++ src/components/list/userInfo/UserInfo.jsx
@@ -1,7 +1,17 @@
 import "./userInfo.css";
 
-export default function UserInfo(){
-    return <div className="user-info">
-        User Info
-    </div>
+export default function UserInfo() {
+    return (
+        <div className="user-info">
+            <div className="user">
+                <img src="./avatar.png" alt="User avatar" />
+                <h2>John Doe</h2>
+            </div>
+            <div className="icons">
+                <img src="./more.png" alt="More options" />
+                <img src="./video.png" alt="Video call" />
+                <img src="./edit.png" alt="Edit profile" />
+            </div>
+        </div>
+    );
 }
