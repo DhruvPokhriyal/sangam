86db6481f6fcbd853b2791e23d1539fc815ac3ca
DhruvPokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2025-07-08 06:59:39 +0000

    Setup HTML for ChatList

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,5 +1,11 @@
 export default function ChatList(){
     return <div className="chatList">
-        ChatList
+        <div className="search">
+            <div className="searchBar">
+                <img src="/search.png"></img>
+                <input type="text" placeholder="Search"></input>
+            </div>
+            <img src="/plus.png"></img>
+        </div>
     </div>
 }
