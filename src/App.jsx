48fd4832022485e95e917fbe5ad9164c11c9f1ae
DhruvPokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2025-07-11 00:51:47 +0530

    Install zustand

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ function App() {
             <div className="container">
                 {user ? (
                     <>
-                        {" "}
                         <List></List>
                         <Chat></Chat>
                         <Detail></Detail>
