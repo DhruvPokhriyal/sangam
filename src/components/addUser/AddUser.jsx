083b6cd01f73ab37f51cb2ed6e09996b267debdd
DhruvPokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2025-07-12 06:26:09 +0530

    feat: Implement user search functionality in AddUser component
    This update adds a search feature that allows users to find existing users by username. It integrates Firestore queries to fetch user data and displays results or a message if no users are found.

--- src/components/addUser/AddUser.jsx
+++ src/components/addUser/AddUser.jsx
@@ -1,20 +1,48 @@
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../../lib/firebase";
 import "./addUser.css";
+import { useState } from "react";
 
 export default function AddUser() {
+    const [users, setUsers] = useState([]);
+    const [empty, setEmpty] = useState(false);
+
+    async function handleSearch(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const username = formData.get("username");
+        console.log(username);
+        try {
+            const usersRef = collection(db, "users");
+            const q = query(usersRef, where("username", "==", username));
+            const querySnapshot = await getDocs(q);
+            if (!querySnapshot.empty) {
+                setUsers(querySnapshot.docs.map((doc) => doc.data()));
+            } else {
+                setEmpty(true);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <>
             <div className="addUser">
-                <form action="">
+                <form onSubmit={handleSearch}>
                     <input type="text" placeholder="Username" name="username" />
                     <button>Search</button>
                 </form>
-                <div className="user">
-                    <div className="detail">
-                        <img src="./avatar.png" alt="" />
-                        <span>John Doe</span>
+                {empty && <p>No users found</p>}
+                {!empty && users.map((user) => (
+                    <div className="user" key={user.id}>
+                        <div className="detail">
+                            <img src={user.avatar || "./avatar.png"} alt="" />
+                            <span>{user.username}</span>
+                        </div>
+                        <button>Add User</button>
                     </div>
-                    <button>Add User</button>
-                </div>
+                ))}
             </div>
         </>
     );
