ebc5702ea63ba54427700b99f8b2fe20900d117d
DhruvPokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2025-07-13 23:01:25 +0530

    feat: Refactor App component to utilize Tailwind CSS for styling
    This commit updates the App component to use Tailwind CSS classes, enhancing the layout and design for improved responsiveness and user experience. The previous CSS styles have been commented out in index.css, streamlining the component's styling with utility classes.

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,25 @@ import Login from "./components/login/Login";
 import Notification from "./components/notification/Notification";
 import { useAuth } from "./hooks/useAuth";
 import { useChatStore } from "./hooks/useChatStore";
+
 function App() {
     const { user, loading } = useAuth();
     const { chatId, receiver } = useChatStore();
     console.log(chatId, receiver);
+    
     if (loading) {
-        return <div className="loading">Loading...</div>;
+        return (
+            <div className="flex items-center justify-center min-h-screen bg-[url('/bg.jpg')] bg-cover bg-center bg-no-repeat">
+                <div className="p-8 sm:p-12 text-2xl sm:text-3xl lg:text-4xl rounded-lg bg-slate-900/90 text-white backdrop-blur-sm">
+                    Loading...
+                </div>
+            </div>
+        );
     }
 
     return (
-        <>
-            <div className="container">
+        <div className="flex items-center justify-center min-h-screen bg-[url('/bg.jpg')] bg-cover bg-center bg-no-repeat text-white">
+            <div className="w-[95vw] sm:w-[90vw] md:w-[85vw] lg:w-[80vw] xl:w-[75vw] 2xl:w-[70vw] h-[85vh] sm:h-[90vh] md:h-[92vh] rounded-xl bg-slate-900/75 backdrop-blur-lg backdrop-saturate-180 border border-white/[0.125] flex overflow-hidden">
                 {user ? (
                     <>
                         <List></List>
@@ -35,7 +43,7 @@ function App() {
                 )}
                 <Notification></Notification>
             </div>
-        </>
+        </div>
     );
 }
 
