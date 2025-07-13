93a59fa95acb6ab1e649b8194c1c4dec33acd6fd
DhruvPokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2025-07-13 10:44:29 +0530

    feat: Add logout functionality to UserInfo component
    This update introduces a logout button in the UserInfo component, allowing users to log out of their account. The logout function is integrated from the useAuth hook, enhancing user control over their session.

--- src/components/list/userInfo/UserInfo.jsx
+++ src/components/list/userInfo/UserInfo.jsx
@@ -1,9 +1,14 @@
 import "./userInfo.css";
 import { useAuth } from "../../../hooks/useAuth";
 
+
 export default function UserInfo() {
 
-    const { user } = useAuth();
+    const { user, logout } = useAuth();
+
+    function handleLogout() {
+        logout(); 
+    }
 
     return (
         <div className="user-info">
@@ -11,11 +16,9 @@ export default function UserInfo() {
                 <img src={user.avatar || "./avatar.png"} alt="User avatar" />
                 <h2>{user.username}</h2>
             </div>
-            {/* <div className="icons">
-                <img src="./more.png" alt="More options" />
-                <img src="./video.png" alt="Video call" />
-                <img src="./edit.png" alt="Edit profile" />
-            </div> */}
+            <div className="logout">
+                <button onClick={handleLogout}>Logout</button>
+            </div>
         </div>
     );
 }
